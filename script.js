d7fc4337f83ff67f6a4c8ded2158c2ff301b95df
yolandacalegari-creator <yolanda.calegari@escola.pr.gov.br>
2026-09-14 11:27:52 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+const botoes = document.querySelectorAll("button");
 
+  botoes.forEach(function (botao) {
+      let curtiu = false;
+      botao.addEventListener("click", botaoClicado);
+      function botaoClicado() {
+        console
